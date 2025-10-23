fdc2ab3f959eb543cfabf7c1a04b316756c1723a
hardwak <ymhere2@gmail.com>
2025-10-23 14:31:52 +0200

    Initial commit

---
+++ backend/src/main/java/com/pwr_zpi/reservespotapi/ReserveSpotApiApplication.java
@@ -0,0 +1,13 @@
+package com.pwr_zpi.reservespotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveSpotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveSpotApiApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/pwr_zpi/reservespotapi/ReserveSpotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwr_zpi.reservespotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveSpotApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
