a2cd4304dc7a13a49266dc81d075dbbad706d68f
DarZen <nivnik004@gmail.com>
2025-10-26 17:15:36 +0100

    Created test NavController and two test screens.

--- frontend/app/src/main/java/com/pwr_zpi/reservespotapp/MainActivity.kt
+++ frontend/app/src/main/java/com/pwr_zpi/reservespotapp/MainActivity.kt
@@ -4,44 +4,63 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.pwr_zpi.reservespotapp.ui.theme.ReserveSpotAppTheme
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ReserveSpotAppTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val navController = rememberNavController()
+
+            NavHost(
+                navController = navController,
+                startDestination = "screentest1"
+            ) {
+                composable("screentest1") { ScreenTest1(navController) }
+                composable(
+                    "screentest2/{input}",
+                    arguments = listOf(navArgument("input") {type = NavType.StringType})
+                ) {
+                    backStackEntry ->
+                    val userInput = backStackEntry.arguments?.getString("input")
+                    ScreenTest2(navController, userInput)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ReserveSpotAppTheme {
-        Greeting("Android")
-    }
-}
+
+
+// Remind the structure
+
+//ReserveSpotAppTheme {
+//    Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+//        Greeting(
+//            name = "Android",
+//            modifier = Modifier.padding(innerPadding)
+//        )
+//    }
+//}
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    ReserveSpotAppTheme {
+//        Greeting("Android")
+//    }
+//}
